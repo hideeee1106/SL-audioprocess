45563ae92d97be0d55ed7dd85aea01a560ef59a6
hideeee <hideeee1106@gmail.com>
2025-05-16 08:53:44 +0800

    add version.h

---
+++ version.h
@@ -0,0 +1,20 @@
+//
+// Created by hideeee on 2025/5/16.
+//
+
+#ifndef NKF_MNN_DEPLOY_R328_VERSION_H
+#define NKF_MNN_DEPLOY_R328_VERSION_H
+
+
+//通用版  版本号
+
+namespace ver {
+    const static int X = 0;
+
+    const static int Y = 0;
+
+    const static int Z = 4;
+
+}
+
+#endif //NKF_MNN_DEPLOY_R328_VERSION_H
